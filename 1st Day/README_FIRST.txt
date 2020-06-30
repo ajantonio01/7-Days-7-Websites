badc74e9b51eb6a966dfb8c4152551898f71473d
ajantonio01 <aantonio@student.apc.edu.ph>
2020-06-30 18:51:17 +0800

    Completed

---
+++ 1st Day/README_FIRST.txt
@@ -0,0 +1,7 @@
+This text file indicates user stories of 1st-day project
+
+User stories:
+- User can see a box which has a border-radius property applied to it
+- User can change the 4 border-radius values that are applied to the box 
+(top-left, top-right, bottom-left, bottom-right)
+- User can copy the resulting CSS to the clipboard
---
+++ 1st Day/js/script.js
@@ -0,0 +1,38 @@
+//Get the id of the box, so that JavaScript can recognize this element
+let borderModify = document.getElementById('borderModify');
+
+//Get the id of the input box that displays results after modifying the border-radius values
+let resultBorderRadius = document.getElementById('resultCSS');
+
+let inputTopLeft = document.getElementById('top-left');
+let inputTopRight = document.getElementById('top-right');
+let inputBottomLeft = document.getElementById('bottom-left');
+let inputBottomRight = document.getElementById('bottom-right');
+
+//Modify the 4 border-radius values of the box
+
+modifyBorderRadius = () => {
+    borderModify.style.borderTopLeftRadius = inputTopLeft.value + 'px';
+    borderModify.style.borderTopRightRadius = inputTopRight.value + 'px';
+    borderModify.style.borderBottomLeftRadius = inputBottomLeft.value + 'px';
+    borderModify.style.borderBottomRightRadius = inputBottomRight.value + 'px';
+
+    resultBorderRadius.value = inputTopLeft.value + ' ' + inputTopRight.value + ' '
+        + inputBottomLeft.value + ' ' + inputBottomRight.value;
+}
+
+//Copy text to clipboard
+copyText = () => {
+    resultBorderRadius.select();
+    document.execCommand("copy");
+}
+
+
+
+
+
+
+
+
+
+//borderModify.style.borderTopLeftRadius = '35px';
